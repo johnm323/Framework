f07063b1aefcd20713e9e3aaa0142e0f83d68753
johnjoseph_m <john.m323@gmail.com>
2019-04-22 20:55:58 +0530

    Formatted

--- Framework/src/test/java/com/learnautomation/pages/LoginPage.java
+++ Framework/src/test/java/com/learnautomation/pages/LoginPage.java
@@ -7,32 +7,28 @@ import org.openqa.selenium.support.FindBy;
 public class LoginPage {
 
 	WebDriver driver;
-	
-	public LoginPage(WebDriver ldriver){
-		this.driver =ldriver;
-		
+
+	public LoginPage(WebDriver ldriver) {
+		this.driver = ldriver;
+
 	}
 
-	
-	@FindBy(name="email") WebElement username;
-	
-	@FindBy(name="password") WebElement password;
-	
-	@FindBy(xpath="//div[@class='ui fluid large blue submit button']") WebElement loginbutton;
-	
-	
-	public void logintocrm(String usernameapplication, String Passwordapplication) throws InterruptedException{
-		
+	@FindBy(name = "email")
+	WebElement username;
+
+	@FindBy(name = "password")
+	WebElement password;
+
+	@FindBy(xpath = "//div[@class='ui fluid large blue submit button']")
+	WebElement loginbutton;
+
+	public void logintocrm(String usernameapplication, String Passwordapplication) throws InterruptedException {
+
 		Thread.sleep(2000);
-		
+
 		username.sendKeys(usernameapplication);
 		password.sendKeys(Passwordapplication);
 		loginbutton.click();
 	}
-	
 
-	
-	
 }
-
-
